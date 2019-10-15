f558a6bf0818088f1a2c511db0c1a0eae15abee3
Migara <evamarcod@gmail.com>
2019-10-15 17:11:22 +0200

    Add docs folder

---
+++ docs/precache-manifest.6fb7ce6c681c763fc9141307afd58c2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "297e8d37babbbadc10038020927ab81f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d6d0da578d39e73410e1",
+    "url": "./static/css/main.37db3271.chunk.css"
+  },
+  {
+    "revision": "a32344c2a453950d4296",
+    "url": "./static/js/2.71ebd0a5.chunk.js"
+  },
+  {
+    "revision": "d6d0da578d39e73410e1",
+    "url": "./static/js/main.143b98c5.chunk.js"
+  },
+  {
+    "revision": "3d2000f8d84d13ec4aa0",
+    "url": "./static/js/runtime-main.23ac2e0e.js"
+  },
+  {
+    "revision": "023d617a1b733e2862bc8d439cface20",
+    "url": "./static/media/pokemon.023d617a.png"
+  }
+]);
