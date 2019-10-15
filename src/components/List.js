dac83dfc71c40fca94d868b5785c1e4a1ed90881
Migara <evamarcod@gmail.com>
2019-10-15 10:12:38 +0200

    Initial commit

---
+++ src/components/List.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const List = props => {
+  const {item} = props;
+  return(
+    <div className="card">
+
+    </div>
+  );
+}
+List.propTypes = {
+  
+}
+export default List;
